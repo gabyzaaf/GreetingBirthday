1fd8e7cea54f1c011b0e50ec7301f87ba5001ffa
gabriel <gabriel.zaafrani@lcdlv.com>
2017-11-07 14:11:12 +0100

    Read file from CSV

--- FileExtensionTest/ReadCsvFile.cs
+++ FileExtensionTest/ReadCsvFile.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using BirthdayGreeting2;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NFluent;
@@ -31,30 +33,47 @@ namespace FileExtensionTest
             Employee employeeEstimate = new Employee("zaafrani","gabriel","gabriel.zaafrani@gmail.com",new DateTime(1990,09,29));
             //RUN
             string file = null;
-            var employee = ReadEmployeeFile(file);
+            file = "Employees.csv";
+            var employee = ReadEmployeesFile(file);
             //ASSERT
-            Check.That(employee).IsEqualTo(employeeEstimate);
+            Check.That(employee[0]).IsEqualTo(employeeEstimate);
         }
 
-        private Employee ReadEmployeeFile(string file)
+        [Test]
+        public void Should_Extract_Multiples_Employee()
         {
-            file = "Employees.csv";
+            List<Employee> employeesEstimate = new List<Employee>()
+            {
+                new Employee("zaafrani","gabriel","gabriel.zaafrani@gmail.com",new DateTime(1990,09,29)),
+                new Employee("zaafrani","michael", "michael.zaafrani@gmail.com",new DateTime(1990,09,09))
+            };
+            var file = "Employees.csv";
+            List<Employee> employees = ReadEmployeesFile(file);
+            Check.That(employees.SequenceEqual(employeesEstimate) && employeesEstimate.SequenceEqual(employees))
+                .IsTrue();
+        }
+
+        
+
+        private List<Employee> ReadEmployeesFile(string file)
+        {
+            List<Employee> employees = new List<Employee>();
+
             var csvLines = File.ReadAllLines(file);
+            foreach (var line in csvLines)
+            {
+                var lineSplited = line.Split(';');
+                string name = lineSplited[0];
+                string firstname = lineSplited[1];
+                string email = lineSplited[2];
 
-            var lineSplited = csvLines[0].Split(';');
-            string name = lineSplited[0];
-            string firstname = lineSplited[1];
-            string email = lineSplited[2];
-
-            var dateSplited = lineSplited[3].Split('/');
-            int year = int.Parse(dateSplited[0]);
-            int month = int.Parse(dateSplited[1]);
-            int day = int.Parse(dateSplited[2]);
-
-           
-
-            Employee employee = new Employee(name, firstname, email, new DateTime(year, month, day));
-            return employee;
+                var dateSplited = lineSplited[3].Split('/');
+                int year = int.Parse(dateSplited[0]);
+                int month = int.Parse(dateSplited[1]);
+                int day = int.Parse(dateSplited[2]);
+                employees.Add(new Employee(name, firstname, email, new DateTime(year, month, day)));
+            }
+            return employees;
         }
     }
 }
